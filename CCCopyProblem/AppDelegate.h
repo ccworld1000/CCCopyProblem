35d4b9e67a3cb876b068b35bfebe33df87a6cd82
CC (deng you hua) <ccworld1000@gmail.com>
2020-12-02 12:33:21 +0800

    CCAutoTag push : copy 1

---
+++ CCCopyProblem/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  CCCopyProblem
+//
+//  Created by CC on 2020/12/2.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ CCCopyProblem/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  CCCopyProblem
+//
+//  Created by CC on 2020/12/2.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
